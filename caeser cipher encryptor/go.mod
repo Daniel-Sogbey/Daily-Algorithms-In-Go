33d1b95cf77bbb520e21ce0f1ec2651cdbc5b231
Daniel-Sogbey <mathematics06physics@gmail.com>
2023-07-14 01:29:57 +0000

    Caeser Cipher Encryptor 😔 solution 1

---
+++ caeser cipher encryptor/go.mod
@@ -0,0 +1,3 @@
+module caeser_cipher_encryptor
+
+go 1.20
---
+++ caeser cipher encryptor/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Hello, world!")
+	letters := "xyz"
+	key := 2
+	fmt.Println(caeserCipherEncryptor(letters, key))
+}
+
+// solution 1 : O(n) Time | O(n) Space
+func caeserCipherEncryptor(str string, key int) string {
+	var newLetters []string
+	var newKey int = key % 26
+
+	for _, letter := range str {
+		newLetters = append(newLetters, string(getNewLetter(letter, newKey)))
+	}
+
+	return strings.Join(newLetters, "")
+}
+
+func getNewLetter(r rune, i int) rune {
+	newLetterCode := int(r) + i
+
+	if newLetterCode <= 122 {
+		return rune(newLetterCode)
+	} else {
+		return rune(96 + newLetterCode%122)
+	}
+
+}
