89001ffe71c1b397d9b84752f7fb3e3c60c2095e
Daniel-Sogbey <mathematics06physics@gmail.com>
2023-08-02 15:12:39 +0000

    table test for two number sum 🧪

--- TwoNumberSums/twonumbersum_test.go
+++ TwoNumberSums/twonumbersum_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestTwoNumberSum(t *testing.T) {
 	array := []int{-3, -1, -4, 5, 6, 7, 10, 11}
@@ -12,4 +14,48 @@ func TestTwoNumberSum(t *testing.T) {
 	if len(outPut) != len(expectedOutput) {
 		t.Errorf("Expect the output to be of length %v, but got %v", len(expectedOutput), len(outPut))
 	}
+}
+
+type testCase struct {
+	input  []int
+	output []int
+	target int
+	isErr  bool
+}
+
+func TestTwoSum(t *testing.T) {
+
+	testCases := []testCase{
+		{
+			input:  []int{-3, -1, -4, 5, 6, 7, 10, 11},
+			output: []int{-1, 11},
+			target: 10,
+			isErr:  false,
+		},
+		{
+			input:  []int{-3, 1, 4, 5, 6, 7, 10, 11},
+			output: []int{1, 7},
+			target: 8,
+			isErr:  false,
+		},
+	}
+
+	for _, tt := range testCases {
+		output := twoNumberSum1(tt.input, tt.target)
+
+		if !check(output, tt.input) {
+			t.Errorf("Expected %d, got %d", tt.output, output)
+		}
+	}
+}
+
+func check(a []int, b []int) bool {
+	i := 0
+
+	for j := 0; j < len(b); j++ {
+		if a[i] == b[j] {
+			i++
+		}
+	}
+	return i == len(a)
 }
