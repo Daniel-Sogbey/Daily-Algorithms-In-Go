6e412328d48a517f04d1d8be9056d7390b020f4a
Daniel-Sogbey <mathematics06physics@gmail.com>
2023-08-03 02:30:52 +0000

    Richest Customer Wealth 💰

---
+++ Richest Customer Wealth/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	accounts := [][]int{{2, 8, 7}, {7, 1, 3}, {1, 9, 5}}
+	fmt.Println(maximumWealth(accounts))
+}
+
+func maximumWealth(accounts [][]int) int {
+	highestSum := 0
+	currentSum := 0
+	for i := 0; i < len(accounts); i++ {
+		for j := 0; j < len(accounts[i]); j++ {
+
+			currentSum += accounts[i][j]
+		}
+
+		if currentSum > highestSum {
+			highestSum = currentSum
+		}
+		currentSum = 0
+	}
+	return highestSum
+}
